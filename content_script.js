70c603c6561db0af845cc602c8dd20498b7266b4
Léo Hackin <leohackin@gmail.com>
2013-05-04 22:17:45 -0300

    commit inicial

---
+++ content_script.js
@@ -0,0 +1,6 @@
+function getText(){
+    return document.body.innerText
+}
+function getHTML(){
+    return document.body.outerHTML
+}
---
+++ js/script.js
@@ -0,0 +1,5 @@
+    
+    // CÓDIGO COMPLEXO PRA LER O CONTEÚDO DA PÁGINA
+    // QUE NÃO TIVE TEMPO DE FAZER A TEMPO DA APRESENTAÇÃO ;D
+	
+	$('#imagens').html('<ul><li><img src="http://s2.glbimg.com/gWwFK7IbtrOs9X6nZwCe2txnSic=/top/s.glbimg.com/jo/eg/f/original/2013/05/04/evans.jpg"><br/>http://s2.glbimg.com/gWwFK7IbtrOs9X6nZwCe2txnSic=/top/s.glbimg.com/jo/eg/f/original/2013/05/04/evans.jpg</li></ul>');
